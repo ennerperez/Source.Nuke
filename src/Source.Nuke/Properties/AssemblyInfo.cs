d22c85d61b262b0bc9aa1a7c21a64f7758224b4b
Enner Pérez <ennerperez@gmail.com>
2023-06-03 09:52:50 -0500

    Assembly Info

---
+++ src/Source.Nuke/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Source Nuke")]
+[assembly: AssemblyDescription("Source Nuke")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyCompany("Enner Pérez")]
+[assembly: AssemblyProduct("Source Nuke")]
+[assembly: AssemblyCopyright("Copyright © 2023")]
+
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
